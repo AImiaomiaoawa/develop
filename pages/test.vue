e5a6a388dda6ef188a5cd4108fb334a3a8e95cdb
lt3ae-bingli <159075925+lt3ae-bingli@users.noreply.github.com>
2024-02-22 09:49:15 -0500

    change

---
+++ pages/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <UBreadcrumb :links="links">
+      <template #icon="{ link }">
+        <UIcon :name="link.icon" />
+      </template>
+    </UBreadcrumb>
+  </template>
+  
+  <script setup lang="ts">
+  import type { BreadcrumbLink } from '#ui/types'
+  
+  export interface Props {
+    links: BreadcrumbLink[]
+  }
+  
+  defineProps<Props>()
+  </script>
+  
